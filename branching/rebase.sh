8d3e87b75cf835cc05e423357bc1bf340f0f38b2
Ложкин Александр Валентинович <lozhkinalv@corp.lukoil.com>
2021-08-20 15:28:37 +0500

    git-rebase 1

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -3,7 +3,11 @@
 
 count=1
 for param in "$@"; do
+<<<<<<< HEAD
     echo "\$@ Parameter #$count = $param"
+=======
+    echo "Parameter: $param"
+>>>>>>> 2f356db (git-rebase 1)
     count=$(( $count + 1 ))
 done
 
