b68b8f9a4cf06f7b34fb24c62148a87fc6f8270a
Ложкин Александр Валентинович <lozhkinalv@corp.lukoil.com>
2021-08-20 15:37:23 +0500

    git-rebase 2

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -4,10 +4,14 @@
 count=1
 for param in "$@"; do
 <<<<<<< HEAD
+<<<<<<< HEAD
     echo "\$@ Parameter #$count = $param"
 =======
     echo "Parameter: $param"
 >>>>>>> 2f356db (git-rebase 1)
+=======
+    echo "Next parameter: $param"
+>>>>>>> 1a12cea (git-rebase 2)
     count=$(( $count + 1 ))
 done
 
